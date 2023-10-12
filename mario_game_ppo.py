57f2c2e76c05f231e39b63e515b6602b329c8f82
mattcw9090 <35331340+mattcw9090@users.noreply.github.com>
2023-10-12 11:24:59 +0000

    Update mario_game_ppo.py

--- mario_game_ppo.py
+++ mario_game_ppo.py
@@ -3,13 +3,22 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.evaluation import evaluate_policy
 import os
 import numpy as np
 
+# CONSTANTS
+TIMESTEPS = 20000
+STEPS_TO_SAVE_AND_LOG_MODEL = 10000
+MODEL_DIR = "./train"
+LOG_DIR = "./log"
+LEARNING_RATE = 1e-5
+ENT_COEF = 0.05
 
 class TrainAndLoggingCallback(BaseCallback):
-    def __init__(self, check_freq, save_path, verbose=0):
+    def __init__(self, env, check_freq, save_path, verbose=0):
         super(TrainAndLoggingCallback, self).__init__(verbose)
         self.check_freq = check_freq
         self.save_path = save_path
@@ -27,40 +36,27 @@ class TrainAndLoggingCallback(BaseCallback):
 
 
 env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True)
+env = Monitor(env)
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-done = True
-env.reset()
-JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
-
-# CONSTANTS
-TIMESTEPS = 5000
-MODEL_DIR = "./train"
-LOG_DIR = "./log"
 
 if os.path.exists(MODEL_DIR):  # If train folder exists
     zip_files = [file for file in os.listdir(MODEL_DIR) if file.endswith('.zip')]
     if zip_files:  # If something in train folder
         largest_zip_file = max(zip_files, key=lambda file: int(file.split('_')[0]))
-        trained_steps = int(largest_zip_file.split('_')[0])
         model_path = os.path.join(MODEL_DIR, largest_zip_file)
         model = PPO.load(model_path, env=env)
         print("LOAD SUCCESSFUL\n")
     else:  # If nothing in train folder
-        model = PPO("MlpPolicy", env, learning_rate=1e-4, verbose=1, tensorboard_log=LOG_DIR)
+        model = PPO("MlpPolicy", env, learning_rate=LEARNING_RATE, tensorboard_log=LOG_DIR, ent_coef=ENT_COEF)
 else:  # If train folder doesn't exist
     os.mkdir(MODEL_DIR)
-    model = PPO("MlpPolicy", env, learning_rate=1e-4, verbose=1, tensorboard_log=LOG_DIR)
+    model = PPO("MlpPolicy", env, learning_rate=LEARNING_RATE, tensorboard_log=LOG_DIR, ent_coef=ENT_COEF)
 
 # Create the callback
-callback = TrainAndLoggingCallback(check_freq=10000, save_path="./train")
+callback_instance = TrainAndLoggingCallback(env=env, check_freq=STEPS_TO_SAVE_AND_LOG_MODEL, save_path=MODEL_DIR)
 
-model.learn(total_timesteps=TIMESTEPS, callback=callback, reset_num_timesteps=False)
-
-# vec_env = model.get_env()
-# obs = vec_env.reset()
-#
-# for step in range(500):
-#     action, _ = model.predict(obs)
-#     obs, reward, done, info = vec_env.step(action)
-#
-# env.close()
+while True:
+    done = True
+    env.reset()
+    JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
+    model.learn(total_timesteps=TIMESTEPS, callback=callback_instance, reset_num_timesteps=False)
