56308a76f245d9c45c85ea1f4d77a69a83f839ac
mattcw9090 <35331340+mattcw9090@users.noreply.github.com>
2023-10-15 02:47:01 +0000

    Update mario_game_ppo.py

--- mario_game_ppo.py
+++ mario_game_ppo.py
@@ -8,14 +8,19 @@ from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.evaluation import evaluate_policy
 import os
 import numpy as np
+import csv
 
 # CONSTANTS
 TIMESTEPS = 20000
-STEPS_TO_SAVE_AND_LOG_MODEL = 10000
+STEPS_TO_SAVE_AND_LOG_MODEL = 5000
 MODEL_DIR = "./train"
 LOG_DIR = "./log"
-LEARNING_RATE = 1e-5
-ENT_COEF = 0.05
+LEARNING_RATE = 1e-4
+
+# Global variables for tracking rewards
+reward_sum = 0
+reward_counts = 0
+mean_rewards_graph = []
 
 class TrainAndLoggingCallback(BaseCallback):
     def __init__(self, env, check_freq, save_path, verbose=0):
@@ -28,11 +33,26 @@ class TrainAndLoggingCallback(BaseCallback):
             os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self):
+        global reward_sum, reward_counts, mean_rewards_graph  # declare them as global
         if self.num_timesteps % self.check_freq == 0:
             model_path = os.path.join(self.save_path, f'{self.num_timesteps}_model')
             self.model.save(model_path)
 
+            # Calculate mean reward
+            mean_reward = reward_sum / reward_counts if reward_counts != 0 else 0
+
+            # Append to CSV file
+            with open('mean_rewards.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([self.num_timesteps, mean_reward])
+
+            # Reset reward_sum and reward_counts
+            reward_sum = 0
+            reward_counts = 0
+
         return True
+
+
 class MarioRewardShaping(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
@@ -43,6 +63,7 @@ class MarioRewardShaping(gym.Wrapper):
         return self.env.reset(**kwargs)
 
     def step(self, action):
+        global reward_sum, reward_counts  # declare them as global
         state, reward, done, truncated, info = self.env.step(action)
 
         # Encourage forward movement
@@ -55,32 +76,33 @@ class MarioRewardShaping(gym.Wrapper):
 
         # Reward for level completion
         if info['flag_get']:
-            reward += 100
+            reward += 1000
 
         # Penalty for taking damage
         if self.prev_info.get('status', '') == 'big' and info['status'] == 'small':
-            reward -= 10
+            reward -= 5
+
+        # Penalty for death
+        if self.prev_info.get('life', 2) > info['life']:
+            reward -= 50
 
         # Reward for getting a power-up
         if self.prev_info.get('status', '') == 'small' and info['status'] == 'big':
             reward += 10
 
-        # Time-based penalty
-        reward -= 0.05
-
         # Penalty for falling into pits
-        if info['y_pos'] - self.prev_info.get('y_pos', info['y_pos']) > 10:
+        if info.get('y_pos', 79) < 77:
             reward -= 5
 
-        # Penalize for death
-        if info['life'] < self.prev_info.get('life', 2):
-            reward -= 50
+        # Time penalty
+        reward -= 1
 
-        # Penalize not progressing
-        if info['x_pos'] == self.prev_info.get('x_pos', 0):
-            reward -= 0.1
 
         self.prev_info = info
+
+        reward_sum += reward
+        reward_counts += 1
+
         return state, reward, done, truncated, info
 
 
@@ -98,10 +120,10 @@ if os.path.exists(MODEL_DIR):  # If train folder exists
         model = PPO.load(model_path, env=env)
         print("LOAD SUCCESSFUL\n")
     else:  # If nothing in train folder
-        model = PPO("MlpPolicy", env, learning_rate=LEARNING_RATE, tensorboard_log=LOG_DIR, ent_coef=ENT_COEF)
+        model = PPO("CnnPolicy", env, learning_rate=LEARNING_RATE, tensorboard_log=LOG_DIR)
 else:  # If train folder doesn't exist
     os.mkdir(MODEL_DIR)
-    model = PPO("MlpPolicy", env, learning_rate=LEARNING_RATE, tensorboard_log=LOG_DIR, ent_coef=ENT_COEF)
+    model = PPO("CnnPolicy", env, learning_rate=LEARNING_RATE, tensorboard_log=LOG_DIR)
 
 # Create the callback
 callback_instance = TrainAndLoggingCallback(env=env, check_freq=STEPS_TO_SAVE_AND_LOG_MODEL, save_path=MODEL_DIR)
