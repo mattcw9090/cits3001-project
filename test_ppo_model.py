c7c1f00b102de89416a658fc44579b39605fa310
mattcw9090 <35331340+mattcw9090@users.noreply.github.com>
2023-10-15 02:47:15 +0000

    Update test_ppo_model.py

--- test_ppo_model.py
+++ test_ppo_model.py
@@ -1,23 +1,27 @@
-from stable_baselines3 import PPO
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-from stable_baselines3.common.evaluation import evaluate_policy
 import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from gym.wrappers import GrayScaleObservation
 import os
+import numpy as np
+import csv
 
 # CONSTANTS
 MODEL_DIR = "./train"
 
 # 1. Set up your environment
-env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
+env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode='human')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-
-# Fix for JoypadSpace (based on your training script)
 JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
 
 # Assuming there's a model saved; otherwise, add a check
-model = PPO.load('./train/2140000_model.zip', env=env)
+model = PPO.load('./train/375000_model.zip', env=env)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
